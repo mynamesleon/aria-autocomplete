d2eb027075022f874e848fe703d1521049ba0749
mynamesleon <contact@mynamesleon.com>
2020-09-28 11:18:40 +0100

    update ids to take an id from options

--- src/autocomplete-ids.ts
+++ src/autocomplete-ids.ts
@@ -12,15 +12,17 @@ export default class {
     OPTION_SELECTED: string;
     SR_ANNOUNCEMENTS: string;
 
-    constructor(id: string) {
+    constructor(elementId: string, optionId?: string) {
         index += 1;
-        this.ELEMENT = id;
-        this.PREFIX = `${id || ''}aria-autocomplete-${index}`;
+        this.ELEMENT = elementId;
+        const id = optionId || elementId || '';
+        this.PREFIX = `${id}aria-autocomplete-${index}`;
+
         this.LIST = `${this.PREFIX}-list`;
-        this.INPUT = `${this.PREFIX}-input`;
         this.BUTTON = `${this.PREFIX}-button`;
         this.OPTION = `${this.PREFIX}-option`;
         this.WRAPPER = `${this.PREFIX}-wrapper`;
+        this.INPUT = optionId || `${this.PREFIX}-input`;
         this.SR_ASSISTANCE = `${this.PREFIX}-sr-assistance`;
         this.OPTION_SELECTED = `${this.PREFIX}-option-selected`;
         this.SR_ANNOUNCEMENTS = `${this.PREFIX}-sr-announcements`;
